cb3e20e7cf4b4963cc8721116e60e00b9cee039b
Vladimir Mitev <vmitev2001@abv.bg>
2023-08-09 15:49:21 +0300

    wip: Implement custom registration

---
+++ FitnessFusion-WebProject/FitnessFusion/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿namespace FitnessFusion.Web.Controllers
+{
+    using FitnessFusion.Data.Models;
+    using FitnessFusion.Web.ViewModels.User;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUserStore<ApplicationUser> userStore;
+
+        public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IUserStore<ApplicationUser> userStore)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.userStore = userStore;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            await userManager.SetEmailAsync(user, model.Email);
+            await userManager.SetUserNameAsync(user, model.Email);
+
+            IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+
+            await signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
